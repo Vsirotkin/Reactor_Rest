3dd5f27128290902d63ff1f408ee734776158dc4
Viktor <vsirotkin15@gmail.com>
2022-03-08 10:17:45 +0300

    added classes in models

--- library/authors/models.py
+++ library/authors/models.py
@@ -10,3 +10,21 @@ class Author(models.Model):
     first_name      = models.CharField(max_length=64)
     last_name       = models.CharField(max_length=64)
     birthday_year   = models.PositiveIntegerField()
+    
+    def __str__(self):
+        return f'{self.name} : {self.last_name}'
+
+
+class Biography(models.Model):
+    text = models.text_field()
+    author = models.OneToOneField(Author, on_delete=models.CASCADE)
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=32)
+    authors = models.ManyToManyField(Author)
+
+
+class Artical(models.Model):
+    name = models.CharField(max_length=32)
+    author = models.ForeignKey(Author, models.PROTECT)
