a1acf3c04f906064adc0f3a29bd0c4f15bcabef1
Viktor <vsirotkin15@gmail.com>
2022-03-08 11:42:43 +0300

    added views

--- library/authors/views.py
+++ library/authors/views.py
@@ -1,8 +1,24 @@
 from rest_framework.viewsets import ModelViewSet
-from .models import Author
-from .serializers import AuthorModelSerializer
+from .models import Author, Book, Biography, Artical
+from .serializers import AuthorModelSerializer, BookModelSerializer, BiographyModelSerializer, ArticalModelSerializer
 
 
 class AuthorModelViewSet(ModelViewSet):
+    #renderer_classes = ['StaticHTMLRenderer']
     queryset = Author.objects.all()
     serializer_class = AuthorModelSerializer
+
+
+class BookModelViewSet(ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookModelSerializer
+
+
+class ModelViewSet(ModelViewSet):
+    queryset = Biography.objects.all()
+    serializer_class = BiographyModelSerializer
+
+
+class BookModelViewSet(ModelViewSet):
+    queryset = Artical.objects.all()
+    serializer_class = ArticalModelSerializer
