6905a292cd8c074364721fc65b412b3f802b07e9
Viktor <vsirotkin15@gmail.com>
2022-03-25 14:19:34 +0300

    added serializers for all models

--- library/authors/serializers.py
+++ library/authors/serializers.py
@@ -1,8 +1,14 @@
 #from rest_framework.serializers import HyperlinkedModelSerializer
 from rest_framework import serializers
+# from rest_framework.serializers import StringRelatedField
+from rest_framework.renderers import JSONRenderer
+
 from .models import Author, Article, Book, Biography
 
-
+class SimpleAuthorModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        fields = ['first_name', 'last_name']
 
 class AuthorSerializer(serializers.ModelSerializer):
     class Meta:
@@ -10,6 +16,7 @@ class AuthorSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class BiographySerializer(serializers.ModelSerializer):
+    author = SimpleAuthorModelSerializer()
     class Meta:
         model = Biography
         fields = ['text', 'author']
